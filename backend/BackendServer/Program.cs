b41192b01bf256a891ecbf574e1dbad7a0eed538
Dana Arter <dana.arter@gmail.com>
2026-01-14 18:03:09 +0100

    fix/add-cors-enum-conversion-to-program

--- backend/BackendServer/Program.cs
+++ backend/BackendServer/Program.cs
@@ -3,12 +3,30 @@ using BackendServer;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSignalR();
+builder.Services.AddSignalR()
+    .AddJsonProtocol(options =>
+    {
+        options.PayloadSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter()
+        );
+    });;
 builder.Services.AddHostedService<MachineStateGenerator>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
 app.UseRouting();
+app.UseCors();
 app.MapHub<MachineStateHub>("/machineStateHub");
 
 app.Run();
